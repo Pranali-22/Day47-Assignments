b134c2d212c101729776637d83d490f5b45de1c5
Pranali-22 <lpranali22@gmail.com>
2022-12-05 10:32:48 +0530

    Added: UC3 - AJAX and promise

--- timer.js
+++ timer.js
@@ -1,42 +1,52 @@
-//UC2
+//UC3
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-function makeAJAXCall(methodType, url, callback, async = true, data = null){
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-        console.log("State Changed Called. Ready State: "+
-                    xhr.readyState+" Status:"+xhr.status);
-        if(xhr.readyState === 4){
-            if(xhr.status === 20 || xhr.status === 201){
-                callback(xhr.responseText);
-            } else if (xhr.status >= 400){
-                console.log("handle 400 Client Error or 500 Server Error");
+function makePromiseCall(methodType, url, async = true, data = null){
+    return new Promise(function(resolve, reject){
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function(){
+            console.log("State Changed Called. Ready State: "+
+                        xhr.readyState+" Status:"+xhr.status);
+            if(xhr.status.toString().match('^[4,5][0-9]{2}$')){
+                resolve(xhr.responseText);
+            } else if(xhr.status.toString().match('^[2][0-9]{2}$')){
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("XHR Failed");
             }
         }
-    }
-    xhr.open(methodType, url, async);
-    if(data){
-        console.log(JSON.stringify(data));
-        xhr.setRequestHeader("Content-Type","application/json");
-        xhr.send(JSON.stringify(data));
-    } else xhr.send();
-    console.log(methodType+" request sent to the server");
+
+        xhr.open(methodType, url, async);
+        if(data) {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type","application/json");
+            xhr.send(JSON.stringify(data));
+        } else xhr.send();
+        console.log(methodType+" request sent to the server");
+    });
 }
 
 const getURL = "https://my-json-server.typicode.com/Pranali-22/fakejsondata/db/employee/1";
-function getUserDetails(data){
-    console.log("Get User Data: "+data)
-}
-makeAJAXCall("GET",getURL, getUserDetails);
+makePromiseCall("GET", getURL, true)
+    .then(responseText => {
+        console.log("Get User Data: "+responseText)
+    })
+    .catch(error => console.log("GET Error Status: "+
+                                JSON.stringify(error)));
 
 const deleteURL = "https://my-json-server.typicode.com/Pranali-22/fakejsondata/db/employee/4";
-function userDeleted(data){
-    console.log("User Deleted "+data)
-}
-makeAJAXCall("DELETE",deleteURL, userDeleted, false);
+makePromiseCall("DELETE", deleteURL, false)
+    .then(responseText => {
+        console.log("User Deleted: "+ responseText)
+    })
+    .catch(error => console.log("DELETE Error Status: "+JSON.stringify(error)));
 
 const postURL = "https://my-json-server.typicode.com/Pranali-22/fakejsondata/db/employee";
 const emplData = {"name":"Harry","salary":"5000"};
-function userAdded(data){
-    console.log("User Added: "+data)
-}
-makeAJAXCall("POST",postURL, userAdded, true, emplData);
+makePromiseCall("POST", postURL, true, emplData)
+    .then(responseText => {
+        console.log("User Added: "+ responseText)
+    })
+    .catch(error => console.log("POST Error Status: "+JSON.stringify(error)));
+    
